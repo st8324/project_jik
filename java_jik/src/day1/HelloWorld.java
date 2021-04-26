431160fb790cdc2f00662c43e74bb2b519d636f7
st8324 <stajun@kut.ac.kr>
2021-04-26 17:34:29 +0900

    자바1일차 : 주석 설명 예제

---
+++ java_jik/src/day1/HelloWorld.java
@@ -0,0 +1,17 @@
+package day1;
+
+/**
+ * 도큐먼트 주석
+ * @author green
+ */
+public class HelloWorld {
+	//주석
+	/* 작성일 : 2021-04-26 
+	 * 작성자 : 전일규
+	 * 내용	: 기본 프로젝트 생성 및 테스트
+	 * */
+	public static void main(String[] args) {
+		System.out.println("Hello wolrd!");	 	
+	}
+
+}
