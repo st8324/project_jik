5ee2ada40292983157ce3ee8f365b43f9d5c9e7d
st8324 <stajun@kut.ac.kr>
2021-04-26 17:35:31 +0900

    자바1일차 : 변수명 규칙 예제

---
+++ java_jik/src/day1/Variable2.java
@@ -0,0 +1,18 @@
+package day1;
+
+public class Variable2 {
+
+	public static void main(String[] args) {
+		int num1 = 1;
+		//int num1 = 2; //에러 발생 : 중복 선언
+		//int 1num = 1;	//에러 발생 : 숫자로 시작할 수 없음
+		int num$ = 1;
+		int _num = 1;
+		//int num% = 1;	//에러 발생 : $와_가 아닌 문자 사용
+		//int public = 1;//에러 발생 : 예약어 사용
+		//학생수를 저장하기 위한 변수를 생성해야 한다
+		int a = 5;
+		int studentCount = 5;
+	}
+
+}
