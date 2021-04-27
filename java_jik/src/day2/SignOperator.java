b74ffa5674075c4b492cb2e82e326e1789072be5
st8324 <stajun@kut.ac.kr>
2021-04-27 17:43:32 +0900

    자바2일차 : 부호 연산자 예제

---
+++ java_jik/src/day2/SignOperator.java
@@ -0,0 +1,13 @@
+package day2;
+
+public class SignOperator {
+
+	public static void main(String[] args) {
+		// 부호 연산자 예제
+		int num1 = 10;
+		System.out.println("-num1 = " + (-num1));
+		int num2 = -20;
+		System.out.println("-num2 = " + (-num2));
+	}
+
+}
