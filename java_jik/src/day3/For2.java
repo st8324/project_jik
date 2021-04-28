dfb69a04f18f74997c1257b5d968f289268a2ebf
st8324 <stajun@kut.ac.kr>
2021-04-28 17:29:55 +0900

    자바3일차 : for문 기본예제2

---
+++ java_jik/src/day3/For2.java
@@ -0,0 +1,19 @@
+package day3;
+
+public class For2 {
+
+	public static void main(String[] args) {
+		/* 1부터 5까지 출력하는 예제를 작성하세요.
+		 * 반복횟수 : i는 1부터 5까지 1씩 증가
+		 * 			초기화 : i는 1부터 => i = 1
+		 * 			조건식 : i는 5까지 => i <= 5
+		 * 			증감식 : i는 1씩 증가 => ++i, i++, i+=1, i=i+1
+		 * 규칙성 : i를 출력
+		 * */
+		int i;
+		for(i = 1 ; i<=5 ; ++i ) {
+			System.out.println(i);
+		}
+	}
+
+}
