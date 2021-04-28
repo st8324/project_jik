b7d2b0731075c6d08e11fc98c7a77ef8a830ef76
st8324 <stajun@kut.ac.kr>
2021-04-28 13:57:30 +0900

    자바3일차 : For문 기본 예제1

---
+++ java_jik/src/day3/For1.java
@@ -0,0 +1,12 @@
+package day3;
+
+public class For1 {
+
+	public static void main(String[] args) {
+		/* Hello world 5번 for문으로 출력하는 예제 */
+		int i;
+		for(i = 1; i<= 5; i++) {
+			System.out.println("Hello world");
+		}
+	}
+}
