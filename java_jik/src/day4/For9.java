c677084c8127bbcfed36d7e286e4edb99a5542d0
st8324 <stajun@kut.ac.kr>
2021-04-29 12:59:59 +0900

    자바4일차 : 무한루프 예제

---
+++ java_jik/src/day4/For9.java
@@ -0,0 +1,14 @@
+package day4;
+
+public class For9 {
+
+	public static void main(String[] args) {
+		/* 무한루프 : 반복문이 끝나지 않는 경우를 무한루프라 한다
+		 * */
+		int i = 1;
+		for( ; i < 5 ; ) {
+			System.out.println("Hello world!");
+		}
+	}
+
+}
