3e62cfd0e726957aba34c8b71d56ccfd0d2187f3
st8324 <stajun@kut.ac.kr>
2021-04-30 17:38:43 +0900

    반복문 : while문을 이용한 구구단 연습문제

---
+++ java_jik/src/day5/Test5_1.java
@@ -0,0 +1,13 @@
+package day5;
+
+public class Test5_1 {
+
+	public static void main(String[] args) {
+		/* while문을 이용하여 구구단 7단을 출력하는 코드를 작성하세요. */
+		int i = 1;
+		while( i <= 9) {
+			System.out.println(" 7 x "+ i +" = " + 7*i);	
+			i += 1;
+		}
+	}
+}
