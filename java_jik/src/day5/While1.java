1dd0ef0f8211ce5115c10cd3f87ee01e370ae8fd
st8324 <stajun@kut.ac.kr>
2021-04-30 17:35:29 +0900

    반복문 : while문일 이용한 정수 출력 예제

---
+++ java_jik/src/day5/While1.java
@@ -0,0 +1,13 @@
+package day5;
+
+public class While1 {
+
+	public static void main(String[] args) {
+		/* 1부터 5까지 while문을 이용하여 출력하는 예제*/
+		int i = 1;
+		while(i <= 5) {
+			System.out.print(i + " ");
+			i += 1;
+		}
+	}
+}
