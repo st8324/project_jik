861fe164771801ff5413d9da90ae29613c025356
stajun <stajun@naver.com>
2021-05-01 22:31:27 +0900

    과제 : 반복문 별예제

---
+++ java_jik/src/etc/Star2.java
@@ -0,0 +1,43 @@
+package etc;
+
+public class Star2 {
+
+	public static void main(String[] args) {
+		/*     *		i=1	공=4 	*=1
+		 *    ***		i=2	공=3 	*=3
+		 *   *****		i=3	공=2 	*=5
+		 *  *******		i=4	공=1 	*=7
+		 * *********	i=5	공=0 	*=9
+		 * 					공=5-i	*=2*i-1
+		 * */
+		int i,k;
+		int num = 5;
+		for(i = 1; i<=num; i+=1) {
+			for(k = 1; k<=num-i; k+=1 ) {
+				System.out.print(" "); 
+			}
+			for(k = 1; k<=2*i-1; k+=1 ) {
+				System.out.print("*"); 
+			}
+			System.out.println();
+		}
+		System.out.println("=========");
+		/* *********	i=1	공=0		*=9
+		 *  *******		i=2	공=1		*=7
+		 *   *****		i=3	공=2		*=5
+		 *    ***		i=4	공=3		*=3
+		 *     *		i=5	공=4		*=1
+		 * 					공=i-1	*=2*(5-i) + 1
+		 */
+		for(i = 1; i<=num; i+=1) {
+			for(k = 1; k<=i-1; k+=1 ) {
+				System.out.print(" "); 
+			}
+			for(k = 1; k<=2*(5-i)+1; k+=1 ) {
+				System.out.print("*"); 
+			}
+			System.out.println();
+		}
+	}
+
+}
