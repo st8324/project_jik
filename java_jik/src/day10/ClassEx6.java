88b4f44429bd02108dbab456fb0cbff6b59de190
st8324 <stajun@kut.ac.kr>
2021-05-10 17:34:37 +0900

    Rect 클래스 활용 예제

---
+++ java_jik/src/day10/ClassEx6.java
@@ -0,0 +1,25 @@
+package day10;
+
+public class ClassEx6 {
+
+	public static void main(String[] args) {
+		
+		Rect r1 = new Rect();
+		r1.print();
+		
+		Rect r2 = new Rect(0, 10, 10, 0);
+		r2.print();
+		
+		Point lu = new Point(0,20);
+		Point rd = new Point(20,0);
+		Rect r3 = new Rect(lu,rd);
+		r3.print();
+		lu.setX(-10);//점을 수정했더니 사각형의 점의 위치가 같이 수정되는 문제가 발생
+		r3.print();
+		
+	}
+}
+
+
+
+
