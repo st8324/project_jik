027e65ef5f053d5ecfd81115870611f4fc336294
st8324 <stajun@kut.ac.kr>
2021-05-17 13:57:55 +0900

    회원 인터페이스 예제

--- java_jik/src/day15/UserService.java
+++ java_jik/src/day15/UserService.java
@@ -8,4 +8,25 @@ public interface UserService {
 	 * 메소드명 : login
 	 * */
 	boolean login(String id, String pw);
+	/* 기능	  : 회원의 아이디, 비밀번호, 이메일, 전화번호가 주어지면 
+	 * 		    해당 정보로 회원가입하여 가입 여부를 알려준 메소드
+	 * 매개변수 : 회원아이디, 비밀번호, 이메일, 전화번호
+	 * 			=> 회원 정보 => User user
+	 * 리턴타입 : 회원가입 성공 여부 => boolean
+	 * 메소드명 : signup
+	 * */
+	boolean signup(User user);
+	/* 기능	  : 회원의 이메일, 전화번호를 수정하는 메소드
+	 * 매개변수 : 회원의 이메일, 전화번호
+	 * 			=> 회원 정보 => User user
+	 * 리턴타입 : 없음 => void
+	 * 메소드명 : modifyUser
+	 * */
+	void modifyUser(User user);
+}
+class User{
+	String id;
+	String pw;
+	String email;
+	String num;
 }
