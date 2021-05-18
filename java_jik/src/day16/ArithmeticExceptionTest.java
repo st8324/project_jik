6bcb1a4b64dc3607c74821e1756a20d07d3d1dfd
st8324 <stajun@kut.ac.kr>
2021-05-18 17:15:45 +0900

    실행 예외 예제

---
+++ java_jik/src/day16/ArithmeticExceptionTest.java
@@ -0,0 +1,9 @@
+package day16;
+
+public class ArithmeticExceptionTest {
+	public static void main(String[] args) {
+		// 정수/정수에서 0으로 나눌 때 발생
+		int num1 = 1, num2 = 0;
+		System.out.println(num1 / num2);
+	}
+}
