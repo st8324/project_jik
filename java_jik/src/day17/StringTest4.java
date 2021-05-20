adef3026c65c543f767f3cb2dc377e88fa6bc8eb
st8324 <stajun@kut.ac.kr>
2021-05-20 10:51:28 +0900

    String클래스 replace()메소드 예제

---
+++ java_jik/src/day17/StringTest4.java
@@ -0,0 +1,11 @@
+package day17;
+
+public class StringTest4 {
+
+	public static void main(String[] args) {
+		System.out.println("제이름은 홍길동입니다.".replace("홍길동", "임꺽정"));
+		/* 문자열C를 입력받고, 해당 단어에 문자열 A와 문자열 B를 입력받아
+		 * 문자열C에 있는 문자열 A를 문자열 B로 바꾸는 코드를 작성하세요. */
+	}
+
+}
