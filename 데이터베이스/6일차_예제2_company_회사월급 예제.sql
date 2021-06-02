7637d2dc034157a40ee1a22080e0f44d98f6fb5a
st8324 <stajun@kut.ac.kr>
2021-06-02 17:49:04 +0900

    DB 6일차 예제

--- 데이터베이스/6일차_예제2_company_회사월급 예제.sql
+++ 데이터베이스/6일차_예제2_company_회사월급 예제.sql
@@ -20,8 +20,8 @@ use company2;
 
 create table if not exists salary(
 	sa_level char(2) not null, 
-    em_base_salary int not null default 0,
-    em_add_salary int not null default 0,
+    sa_base_salary int not null default 0,
+    sa_add_salary int not null default 0,
     primary key(sa_level)
 );
 
@@ -54,10 +54,51 @@ insert into employee
 drop procedure if exists update_level;
 delimiter //
 create procedure update_level(
-    
+	in _num int,
+    in _level char(2),
+    in _year int
 )
 begin
-	
+	-- 해당 사원의 월급을 변경
+    declare _base int default 0;	-- 기본급 변수
+    declare _add int default 0;		-- 호봉 추가금 변수
+    declare _salary int default 0;	-- 월급
+    -- 기본급 가져오기
+    set _base = (select sa_base_salary from salary where sa_level = _level);
+    -- 호봉 기본급 가져오기
+    set _add = (select sa_add_salary from salary where sa_level = _level);
+    -- 연차에 맞게 호봉 추가금 계산하기
+    set _add = _add * _year;
+    -- 사원의 직급과 년차, 월급을 수정
+    update employee set 
+		em_sa_level = _level,
+        em_year = _year,
+        em_salary = _base + _add
+        where em_num = _num;
 end //
 delimiter ;
-call update_level();
+call update_level(1, '과장', 1);
+
+-- 사원에게 월급을 지급하는 프로시저를 생성하세요. 월급 지급액은 월급에서 세금 10%를 제외한 금액
+drop procedure if exists insert_payments;
+delimiter //
+create procedure insert_payments(
+	in _num int
+)
+begin
+	-- payments 테이블 insert
+    declare _price int default 0;
+    -- 사원의 월급 금액을 가져옴
+    set _price = (select em_salary from employee where em_num = _num);
+    -- 세금을 제외
+    set _price = _price * 0.9;
+    -- payments 테이블에 지급 내역을 추가
+    insert into payments(pa_em_num, pa_price) values(_num, _price);
+end //
+delimiter ;
+call insert_payments(1);
+select * from payments;
+
+
+
+
