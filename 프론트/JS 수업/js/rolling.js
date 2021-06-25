ed5ad12d2998c5269f6fd89eb22975c1c8392ea2
st8324 <stajun@kut.ac.kr>
2021-06-25 09:46:24 +0900

    JS 6일차 : 수업내용

--- 프론트/JS 수업/js/rolling.js
+++ 프론트/JS 수업/js/rolling.js
@@ -22,3 +22,26 @@ function rollingLeft(list, listItem, animateTime, intervalTime){
 	},1500);
 	return id;
 }
+function rollingRight(list, listItem, animateTime, intervalTime){
+	var id = setInterval(function(){
+		if(!$(list + ' ' + listItem).first().is(':animated')){
+			var width = $(list + ' ' + listItem).last().width();
+			$(list + ' ' + listItem).last().detach().prependTo(list)
+				.css('margin-left',-width+'px')
+				.animate({'margin-left':'0px'},animateTime);
+		}
+	},intervalTime);
+	return id;
+}
+function rolling(direction, list, listItem, margin, animateTime, intervalTime){
+	
+	if(direction.toLowerCase() == 'left'){
+		return rollingLeft(list,listItem,animateTime,intervalTime);	
+	}
+	else if(direction.toLowerCase() == 'right'){
+		return rollingRight(list,listItem,animateTime,intervalTime);
+	}
+	else{
+		return rollingTop(list,listItem,margin,animateTime,intervalTime);
+	}
+}
